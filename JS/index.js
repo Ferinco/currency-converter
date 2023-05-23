b6ef718c437ef95a2416fbd69f5be51d93cd1765
Ferinco <emmanuelferrum003@gmail.com>
2023-05-23 19:43:02 +0100

    initial commit

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,23 @@
-// const apiKey = 'kMXxXFD5oM9NNjAclnnBEgiB6WmGCPN8aWPf8ymi';
-// const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`;
+const apiKey = 'kMXxXFD5oM9NNjAclnnBEgiB6WmGCPN8aWPf8ymi';
+const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`;
 
-// fetch(apiUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Handle the API response
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     // Handle any errors
-//     console.error('Error:', error);
-//   });
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const inputSelect = document.getElementById("input-select")
+    for (const CurrencyCode in data ) {
+      const option = document.createElement('option');
+      option.value = CurrencyCode;
+      option.textContent = `${CurrencyCode}`;
+      inputSelect.appendChild(option);
+      
+    }
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error('Error:', error);
+  });
 
 const input = document.getElementById("input")
  let screen = document.getElementById("screen")
