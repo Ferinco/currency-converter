efe5ce5354225f145ffa9c24d5862dcaa195854f
Ferinco <emmanuelferrum003@gmail.com>
2023-05-23 18:47:35 +0100

    page design

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,28 @@
-const apiKey = 'kMXxXFD5oM9NNjAclnnBEgiB6WmGCPN8aWPf8ymi';
-const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`;
+// const apiKey = 'kMXxXFD5oM9NNjAclnnBEgiB6WmGCPN8aWPf8ymi';
+// const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`;
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Handle the API response
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error('Error:', error);
-  });
+// fetch(apiUrl)
+//   .then(response => response.json())
+//   .then(data => {
+//     // Handle the API response
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     // Handle any errors
+//     console.error('Error:', error);
+//   });
+
+const input = document.getElementById("input")
+ let screen = document.getElementById("screen")
+const convertBtn = document.getElementById("convertBtn")
+screen.style.marginLeft = "-2000px"
+screen.style.transition = "0.5s"
+
+convertBtn.addEventListener("click", (e)=>{
+  e.preventDefault()
+  let inputValue = input.value.trim()
+  console.log(inputValue)
+  outputValue = inputValue * 750
+  screen.style.marginLeft = "0px"
+  screen.innerHTML = `${outputValue}`
+})
